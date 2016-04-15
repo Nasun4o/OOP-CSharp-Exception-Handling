2d9cbcd6a67056e818068caea367edf4a651b92c
nasun4o <nasunawow@abv.bg>
2016-04-15 12:45:54 +0300

    HomeWorkExceptionHandling.sln

---
+++ EnterNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+//Здравйте приятели понеже нас малко ни прецакаха и ни пуснаха от Basic направо на ООП 
+//ще се радвам ако ми посочите грешките и начинът по който мога да ги поправя, Благодаря !!
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int start = 1;
+            int end = 100;
+            int[] number = new int[10];
+
+
+            for (int i = 0; i < number.Length; i++)
+            {
+
+                number[i] = ReadNumbers(start + 1, end - (number.Length - i));
+                start = number[i];
+            }
+            Console.WriteLine("Your number are: ");
+            Console.WriteLine(string.Join(",", number));
+        }
+        public static int ReadNumbers(int start, int end)
+        {
+            try
+            {
+                string userInput = Console.ReadLine();
+                int num;
+                bool succesParsed = int.TryParse(userInput, out num);
+                if (!succesParsed || num < start || num > end)
+                {
+                    throw new ArgumentOutOfRangeException("You need to enter a valid number !!");
+                }
+                return num;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You have enter invalid number , please enter new number!!");
+
+                return ReadNumbers(start, end);
+            }
+        }
+    }
+}
---
+++ SquareRoot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+//Здравйте приятели понеже нас малко ни прецакаха и ни пуснаха от Basic направо на ООП 
+//ще се радвам ако ми посочите грешките и начинът по който мога да ги поправя, Благодаря !!
+
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number = 0;
+            double sqrt = 0;
+            try
+            {
+             number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Number must be a positive");
+                }
+                sqrt = Math.Sqrt(number);
+            Console.WriteLine(sqrt);
+            }
+            catch  (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Number must be positive !!");
+            }
+            catch (FormatException)
+            {
+               
+                Console.WriteLine("Invalid Format the input must be a valid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good Bye");
+            }
+        }
+    }
+}
